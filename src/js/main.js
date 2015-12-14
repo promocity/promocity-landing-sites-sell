b44adee36fc00d619bb868daa1be8fe6bd1630b9
promocity <verst.promocity.ekb@yandex.ru>
2015-12-14 11:10:48 +0500

    release v1.0
    release v1.0

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     lazyLoad : true,
     slideSpeed : 800,
     navigationText : false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 6000,
     pauseOnHover : true
   });
@@ -49,10 +49,69 @@ $(document).ready(function() {
    }
  });
 
+  $('#call-request-form').validate({
+   rules: {
+     name: {
+      required: true,
+      minlength: 4    
+     },
+     tel: {
+      required: true
+     }
+   }, //end rules
+   messages: {
+     name: {
+       required: "Пожалуйста введите Ф.И.О.",
+       minlength: "Ф.И.О. не должны быть короче 4 символов."
+     },
+     tel: {
+        required: "Заполните поле телефон."
+     }
+   }
+ });
+
   $("#header-call-request-btn").click(function(){
     $("#call-request-form").show();
     $(".forms__pop-up-overlay").show();
     return false;
   });
 
+  $(".forms__pop-up-overlay").click(function(){
+    $("#call-request-form").hide();
+    $(".forms__pop-up-overlay").hide();
+    $(".complete-window").hide();
+    return false;
+  });
+
+  $(".forms-pop-up__close-btn").click(function(){
+    $("#call-request-form").hide();
+    $(".forms__pop-up-overlay").hide();
+    $(".complete-window").hide();
+    return false;
+  });
+
+  $("#pop-up-tel").mask("+7 (999) 999-9999");
+
+});
+
+  
+  function compeliteWindow() {
+    $(".forms__pop-up-overlay").show();
+    $("#call-request-form").hide();
+    $(".complete-window").show();
+  }
+
+  function siteVisitCheck1() { $('#request-checkbox-1').prop("checked",true); }
+  function siteVisitCheck2() { $('#request-checkbox-2').prop("checked",true); }
+  function siteVisitCheck3() { $('#request-checkbox-3').prop("checked",true); }
+  function siteVisitCheck4() { $('#request-checkbox-5').prop("checked",true); }
+  function siteVisitCheck5() { $('#request-checkbox-4').prop("checked",true); }
+
+/* 27 = escape */
+$(document).keyup(function(event){
+    if (event.keyCode == 27) {
+      $("#call-request-form").hide();
+      $(".forms__pop-up-overlay").hide();
+      $(".complete-window").hide();
+    }
 });
